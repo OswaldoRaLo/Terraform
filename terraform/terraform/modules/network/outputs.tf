e8a218c33dd3fff6fda2684d4133ebed9d1211fb
OswaldoRaLo <90931465+OswaldoRaLo@users.noreply.github.com>
2024-05-09 20:20:22 -0600

    Delete terraform/terraform directory

--- terraform/terraform/modules/network/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "vpc_id" {
-    description = "El ID de la VPC"
-    value = aws_vpc.main_vpc.id
-}
-
-output "vpc_cidr_block" {
-    description = "El bloque CIDR de la VPC"
-    value = aws_vpc.main_vpc.cidr_block
-}
--- terraform/terraform/modules/subnet/main.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_subnet" "my_subnet" {
-  vpc_id            = var.vpc_id
-  cidr_block        = var.cidr_block
-  availability_zone = var.availability_zone
-}
-
-output "subnet_id" {
-  value = aws_subnet.my_subnet.id
-}
